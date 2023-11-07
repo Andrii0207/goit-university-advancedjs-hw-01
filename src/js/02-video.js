388b2fc7905e1b0d71f34c6202f81698cdf86af4
Andrii <Popov.Andrey80@gmail.com>
2023-11-07 23:31:41 +0200

    add second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from "@vimeo/player"
 
+
+const iframe = document.querySelector('#vimeo-player');
+
+const LS_KEY = "videoplayer-current-time";
+
+let LS_DATA = JSON.parse(localStorage.getItem(LS_KEY))
+console.log("LS_DATA >>>", LS_DATA)
+
+const player = new Player(iframe);
+
+player.on('play', onPlay)
+
+function onPlay(evt) {
+    console.log("Play event >>>", evt)
+    const { seconds } = evt;
+    localStorage.setItem(LS_KEY, JSON.stringify(seconds))
+}
+
+player.setCurrentTime().then(function (seconds) {
+    console.log('seconds:', seconds);
+    localStorage.setItem(LS_KEY, JSON.stringify(seconds))
+}).catch(function (err) {
+    console.log(err)
+}) 
